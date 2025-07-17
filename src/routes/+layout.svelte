2ae9fd6f1739ee8d5bd121aaf9875cb82bff9453
av275 <av275@duke.edu>
2025-07-17 11:33:13 -0400

    Change the layout page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -13,7 +13,8 @@
     
 
     <div class = "relative">
-      <button class="py-2 px-3 font-semibold hover:underline fixed top-22" onclick={() => gestureDropdown =!gestureDropdown} >Gesture Manual</button>
+      <button class="py-2 px-3 font-semibold hover:underline fixed top-22" 
+      onclick={() => gestureDropdown =!gestureDropdown} >Gesture Manual</button>
 
         {#if gestureDropdown}
           <div class = "flex flex-col pl-3 ">
@@ -23,13 +24,16 @@
               <a class="py-2 px-3 font-semibold hover:underline fixed top-54" href="/gesture-manual/scroll">Scrolling</a>
               <a class="py-2 px-3 font-semibold hover:underline fixed top-62" href="/gesture-manual/drag">Drag</a>
               <a class="py-2 px-3 font-semibold hover:underline fixed top-70" href="/gesture-manual/playground">Playground</a>
+
           </div>
         {/if}
 
     </div>
   </nav>
 
-  <main class="flex-1 overflow-y-auto p-3">
-    {@render children()}
+  <main class="flex-1 overflow-y-auto p-4">
+    <slot/> 
   </main>
+
+
 </div>
